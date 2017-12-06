0477f01c33f280d4c5e4b371476037d81e5cdbbe
jxsome <31810836+jxsome@users.noreply.github.com>
2017-12-06 22:49:21 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -35,6 +35,14 @@
 		}
 
 	};
+	//百度统计
+	var _hmt = _hmt || [];
+	(function() {
+		var hm = document.createElement("script");
+		hm.src = "https://hm.baidu.com/hm.js?792b3f781c005045cfa78d515f457973";
+		var s = document.getElementsByTagName("script")[0]; 
+		s.parentNode.insertBefore(hm, s);
+		})();
 
 	// Animations
 
